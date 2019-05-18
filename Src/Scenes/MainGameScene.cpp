50fa853d62c00efa79a1ca3536c047b4d56ed55f
田中 真依 <rabitfiver@gmail.com>
2019-05-19 04:45:16 +0900

    プレイヤーキャラクターの移動操作を実装にする.

--- Src/Scenes/MainGameScene.cpp
+++ Src/Scenes/MainGameScene.cpp
@@ -41,6 +41,10 @@ bool MainGameScene::Initialize()
   progSkeletalMesh = shaderCache.Create("Res/SkeletalMesh.vert", "Res/SkeletalMesh.frag");
   progSkeletalMesh->BindUniformBlock("MeshMatrixUniformData", 0);
 
+  glm::vec3 startPos(100, 0, 150);
+  startPos.y = heightMap.Height(startPos);
+  meshPlayer->translation = startPos;
+
   return true;
 }
 
@@ -49,6 +53,16 @@ bool MainGameScene::Initialize()
 *
 * @param sceneStack シーン制御オブジェクト.
 * @param deltaTime  前回の更新からの経過時間(秒).
+*
+* TODO: 地面の上を移動.
+* TODO: ジャンプ動作.
+* TODO: 攻撃動作と攻撃判定.
+* TODO: 敵の出現.
+* TODO: 敵の思考.
+* TODO: 木を植える.
+* TODO: 村人と会話.
+* TODO: ボス.
+* TODO: 法力.
 */
 void MainGameScene::Update(SceneStack& sceneStack, float deltaTime)
 {
@@ -61,17 +75,26 @@ void MainGameScene::Update(SceneStack& sceneStack, float deltaTime)
   fontRenderer.EndUpdate();
 
   if (IsActive()) {
+    const glm::vec3 dir(0, 0, -1);
     const glm::vec3 left = glm::normalize(glm::cross(glm::vec3(0, 1, 0), dir));
     const float dt = static_cast<float>(window.DeltaTime());
+    glm::vec3 move(0);
+    const float speed = 5.0f;
     if (window.KeyPressed(GLFW_KEY_W)) {
-      pos += dir * dt * 10.0f;
+      move += dir * dt * speed;
     } else if (window.KeyPressed(GLFW_KEY_S)) {
-      pos -= dir * dt * 10.0f;
+      move -= dir * dt * speed;
     }
     if (window.KeyPressed(GLFW_KEY_A)) {
-      pos += left * dt * 10.0f;
+      move += left * dt * speed;
     } else if (window.KeyPressed(GLFW_KEY_D)) {
-      pos -= left * dt * 10.0f;
+      move -= left * dt * speed;
+    }
+    if (glm::dot(move, move)) {
+      meshPlayer->translation += move;
+      meshPlayer->translation.y = heightMap.Height(meshPlayer->translation);
+      move = glm::normalize(move);
+      meshPlayer->rotation = glm::quat(glm::vec3(0, std::atan2(-move.z, move.x) + glm::radians(90.0f), 0));
     }
 
     const glm::vec2 currentMousePos = GLFWEW::Window::Instance().MousePosition();
@@ -85,8 +108,8 @@ void MainGameScene::Update(SceneStack& sceneStack, float deltaTime)
     if (mouseMove.y) {
       matRY = glm::rotate(glm::mat4(1), mouseMove.y / 100.0f, left);
     }
-    dir = matRX * matRY * glm::vec4(dir, 1);
-    dir = normalize(dir);
+    //dir = matRX * matRY * glm::vec4(dir, 1);
+    //dir = normalize(dir);
 
     meshBuffer.ResetUniformData();
     meshPlayer->Update(deltaTime);
@@ -111,7 +134,8 @@ void MainGameScene::Render()
 {
   const GLFWEW::Window& window = GLFWEW::Window::Instance();
 
-  const glm::mat4 matView = glm::lookAt(pos, pos + dir, glm::vec3(0, 1, 0));
+  const glm::vec3 cameraPos = meshPlayer->translation + glm::vec3(0, 30, 15);
+  const glm::mat4 matView = glm::lookAt(cameraPos, meshPlayer->translation, glm::vec3(0, 1, 0));
   const float aspectRatio = static_cast<float>(window.Width()) / static_cast<float>(window.Height());
   const glm::mat4 matProj = glm::perspective(glm::radians(30.0f), aspectRatio, 1.0f, 1000.0f);
   const glm::mat4 matModel = glm::scale(glm::mat4(1), glm::vec3(1));
@@ -127,8 +151,8 @@ void MainGameScene::Render()
     if (locMeshIndex > 0) {
       progMesh->SetUniformInt(locMeshIndex, 0);
     }
-    meshBuffer.GetMesh("Terrain")->Draw();
-    meshBuffer.GetMesh("Circle")->Draw();
+    meshTerrain->Draw();
+    meshCircle->Draw();
 
     progMesh->Unuse();
   }
@@ -136,7 +160,7 @@ void MainGameScene::Render()
   {
     progSkeletalMesh->Use();
 
-    progSkeletalMesh->SetViewProjectionMatrix(matProj * matView * glm::scale(glm::mat4(1), glm::vec3(10)));
+    progSkeletalMesh->SetViewProjectionMatrix(matProj * matView);
     const GLint locMeshIndex = progSkeletalMesh->GetUniformLocation("meshIndex");
     if (locMeshIndex > 0) {
       progSkeletalMesh->SetUniformInt(locMeshIndex, 0);
