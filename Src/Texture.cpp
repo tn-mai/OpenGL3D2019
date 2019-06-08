f0b1e7a7504bf212e89809bc23e63bdab31d1400
田中 真依 <rabitfiver@gmail.com>
2019-06-08 14:57:51 +0900

    テクスチャフィルタをNEARESTからLINEARに変更.
    小さいテクスチャでドット絵を描くほうが手軽だったのでNEARESTにしていたが、
    今回作成するような3DゲームにはLINEARのほうが向いていると思う.

--- Src/Texture.cpp
+++ Src/Texture.cpp
@@ -552,8 +552,8 @@ GLuint CreateImage2D(GLsizei width, GLsizei height, const GLvoid* data, GLenum f
 
   // テクスチャのパラメータを設定する.
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
   if (format == GL_RED) {
