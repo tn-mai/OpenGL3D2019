b1914ee5422c7b1ef041214b2dfe1ba4480f85bd
田中 真依 <rabitfiver@gmail.com>
2019-06-25 14:02:12 +0900

    テキストに合わせて修正.
    - 適切な関数コメントを追記.
    - 関数の定義位置を調整.
    - 色データをGetColor関数で取得.
    - インデックスデータ作成コードをわかりやすくする.
    - 説明しやすいように、法線計算を近傍平面の平均にする.

--- Src/Terrain.cpp
+++ Src/Terrain.cpp
@@ -3,112 +3,115 @@
 */
 #include "Terrain.h"
 #include "Texture.h"
-#include <fstream>
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
+/// 地形に関するクラス等を格納する名前空間.
 namespace Terrain {
 
 /**
 * 画像ファイルから地形データを読み込む.
+*
+* @param path      画像ファイル名.
+* @param scale     高さに掛ける係数.
+* @param baseLevel 高さ0とみなす値(通常0.0～1.0).
+*
+* @retval true  読み込み成功.
+* @retval false 読み込み失敗.
 */
-bool HeightMap::Load(const char* path, float scale, float middleLevel)
+bool HeightMap::Load(const char* path, float scale, float baseLevel)
 {
   Texture::ImageData imageData;
   if (!Texture::LoadImage2D(path, imageData)) {
     return false;
   }
-  if (imageData.format != GL_RED) {
-    return false;
-  }
+
+  name = path;
   size = glm::ivec2(imageData.width, imageData.height);
+
   // 画像は下から上に向かって格納されているので、上下反転しながら高さデータに変換.
   heights.resize(imageData.data.size());
   for (int y = 0; y < size.y; ++y) {
     for (int x = 0; x < size.x; ++x) {
-      const uint8_t color = imageData.data[y * size.x + x];
-      heights[(size.y - y - 1) * size.x + x] = (color / 255.0f - middleLevel) * scale;
+      const glm::vec4 color = imageData.GetColor(x, y);
+      heights[(size.y - y - 1) * size.x + x] = (color.r - baseLevel) * scale;
     }
   }
   return true;
 }
 
-glm::vec3 HeightMap::CalcNormal(int centerX, int centerZ) const
-{
-  const int minX = std::max(centerX - 1, 0);
-  const int maxX = std::min(centerX + 1, size.x - 1);
-  const int minZ = std::max(centerZ - 1, 0);
-  const int maxZ = std::min(centerZ + 1, size.y - 1);
-  const glm::vec3 center(centerX, heights[centerZ * size.y + centerX], size.y - centerZ);
-  glm::vec3 normal(0);
-  for (int z = minZ; z <= maxZ; ++z) {
-	  if (z == centerZ) {
-		  continue;
-	  }
-    for (int x = minX; x <= maxX; ++x) {
-      if (x == centerX) {
-        continue;
-      }
-      const glm::vec3 p(x, heights[z * size.y + x], size.y - z);
-      const glm::vec3 v0 = normalize(p - center);
-      const glm::vec3 tangent = normalize(cross(glm::vec3(0, 1, 0), v0));
-      normal += normalize(cross(v0, tangent));
-    }
-  }
-  return normalize(normal);
-}
-
-
 /**
 * 高さマップからメッシュを作成する.
+*
+* @param meshBuffer メッシュ作成先となるメッシュバッファ.
+* @param name       作成するメッシュの名前.
+*
+* @retval true  メッシュの作成に成功.
+* @retval false メッシュを作成できなかった.
 *
 * 三角形は以下の形になる.
 *   +--+
 *   |／|
 *   +--+
 */
-bool HeightMap::CreateMesh(Mesh::Buffer& meshBuffer, const char* name)
+bool HeightMap::CreateMesh(Mesh::Buffer& meshBuffer, const char* name) const
 {
   if (heights.empty()) {
     return false;
   }
+
+  // 頂点データを作成.
   Mesh::Vertex v;
   v.color = glm::vec4(1);
   std::vector<Mesh::Vertex> vertices;
   vertices.reserve(size.x * size.y);
   for (int z = 0; z < size.y; ++z) {
     for (int x = 0; x < size.x; ++x) {
+      // テクスチャ座標は上がプラスなので、向きを逆にする必要がある.
       v.position = glm::vec3(x, heights[z * size.x + x], z);
-      v.texCoord = glm::vec2(x, (size.y - 1) - z) / (glm::vec2(size) - 1.0f); // テクスチャ座標は上がプラスなので向きを逆にする必要がある.
+      v.texCoord = glm::vec2(x, (size.y - 1) - z) / (glm::vec2(size) - 1.0f);
       v.normal = CalcNormal(x, z);
       vertices.push_back(v);
     }
   }
   const size_t vOffset = meshBuffer.AddVertexData(vertices.data(), vertices.size() * sizeof(Mesh::Vertex));
 
-  std::vector<GLushort> indices;
+  // インデックスデータを作成.
+  std::vector<GLuint> indices;
   indices.reserve(size.x * size.y);
   for (int z = 0; z < size.y - 1; ++z) {
     for (int x = 0; x < size.x - 1; ++x) {
-      indices.push_back(z * size.x       + x);
-      indices.push_back((z + 1) * size.x + x);
-      indices.push_back(z * size.x       + (x + 1));
-
-      indices.push_back((z + 1) * size.x + (x + 1));
-      indices.push_back(z * size.x + (x + 1));
-      indices.push_back((z + 1) * size.x + x);
-    }
+       const GLuint a = (z + 1) * size.x + x;
+       const GLuint b = (z + 1) * size.x + (x + 1);
+       const GLuint c = z       * size.x + (x + 1);
+       const GLuint d = z       * size.x + x;
+       indices.push_back(a);
+       indices.push_back(b);
+       indices.push_back(c);
+ 
+       indices.push_back(c);
+       indices.push_back(d);
+       indices.push_back(a);    }
   }
-  const size_t iOffset = meshBuffer.AddIndexData(indices.data(), indices.size() * sizeof(GLushort));
+  const size_t iOffset = meshBuffer.AddIndexData(indices.data(), indices.size() * sizeof(GLuint));
 
-  meshBuffer.AddMesh("Terrain", indices.size(), GL_UNSIGNED_SHORT, iOffset, vOffset);
+  // 頂点データとインデックスデータからメッシュを作成.
+  const Mesh::Primitive p = meshBuffer.CreatePrimitive(indices.size(), GL_UNSIGNED_INT, iOffset, vOffset);
+  const Mesh::Material m = meshBuffer.CreateMaterial(glm::vec4(1), nullptr);
+  meshBuffer.AddMesh("Terrain", p, m);
 
   return true;
 }
 
 /**
 * 高さを取得する.
+*
+* @param pos 高さを取得する座標.
+*
+* @return posの位置の高さ.
+*
+* ハイトマップはXZ平面に対応し、原点(0, 0)からプラス方向に定義される.
 */
 float HeightMap::Height(const glm::vec3& pos) const
 {
@@ -142,6 +145,51 @@ float HeightMap::Height(const glm::vec3& pos) const
     const float h2 = heights[index.y * size.x       + (index.x + 1)];
     return h0 + (h1 - h0) * (1.0f - offset.x) + (h2 - h0) * (1.0f - offset.y);
   }
+}
+
+/**
+* 高さ情報から法線を計算する.
+*
+* @param centerX 計算対象のX座標.
+* @param centerY 計算対象のY座標.
+*
+* @return (centerX, centerY)の位置の法線.
+*/
+glm::vec3 HeightMap::CalcNormal(int centerX, int centerZ) const
+{
+  // メッシュは以下のような形状なので、対象頂点を中心とする六角形の各頂点が法線に影響する.
+  // 対象頂点がマップの端にある場合、マップ外の頂点は無視する.
+  //
+  //     +----( 0,-1)-( 1,-1)
+  //     |    ／ |    ／ |
+  //     | ／    | ／    |
+  //  (-1, 0)----+----( 1, 0)
+  //     |    ／ |    ／ |
+  //     | ／    | ／    |
+  //  (-1, 1)-( 0, 1)----+
+  //
+
+  const glm::ivec2 offsetList[] = {
+    { 0,-1}, { 1,-1}, { 1, 0}, { 0, 1}, {-1, 1}, {-1, 0}, { 0,-1}
+  };
+  const glm::vec3 center(centerX, heights[centerZ * size.x + centerX], centerZ);
+  glm::vec3 sum(0);
+  for (size_t i = 0; i < 6; ++i) {
+    glm::vec3 p0(centerX + offsetList[i].x, 0, centerZ + offsetList[i].y);
+    if (p0.x < 0 || p0.x >= size.x || p0.z < 0 || p0.z >= size.y) {
+      continue;
+    }
+    p0.y = heights[static_cast<size_t>(p0.z * size.x + p0.x)];
+
+    glm::vec3 p1(centerX + offsetList[i + 1].x, 0, centerZ + offsetList[i + 1].y);
+    if (p1.x < 0 || p1.x >= size.x || p1.z < 0 || p1.z >= size.y) {
+      continue;
+    }
+    p1.y = heights[static_cast<size_t>(p1.z * size.x + p1.x)];
+
+    sum += normalize(cross(p1 - center, p0 - center));
+  }
+  return normalize(sum);
 }
 
 } // namespace Terrain
--- Src/Terrain.h
+++ Src/Terrain.h
@@ -5,6 +5,7 @@
 #define TERRAIN_H_INCLUDED
 #include "Mesh.h"
 #include <glm/glm.hpp>
+#include <string>
 #include <vector>
 
 namespace Terrain {
@@ -18,13 +19,15 @@ public:
   ~HeightMap() = default;
 
   bool Load(const char* path, float scale, float middleLevel);
-  bool CreateMesh(Mesh::Buffer& meshBuffer, const char* name);
+  bool CreateMesh(Mesh::Buffer& meshBuffer, const char* name) const;
   float Height(const glm::vec3& pos) const;
   const glm::ivec2& Size() const { return size; }
+  const std::string& Name() const { return name; }
 
 private:
-  glm::ivec2 size = glm::ivec2(0);
-  std::vector<float> heights;
+  std::string name; ///< 元になった画像ファイル名.
+  glm::ivec2 size = glm::ivec2(0); ///< ハイトマップの大きさ.
+  std::vector<float> heights; ///< 高さデータ.
 
   glm::vec3 CalcNormal(int x, int z) const;
 };
